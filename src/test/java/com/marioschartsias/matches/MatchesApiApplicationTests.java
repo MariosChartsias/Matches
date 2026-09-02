9637f8cfae901bdbf90649ea783d0d85ae748da6
Marios Chartsias <mariosxama@gmail.com>
2026-09-02 21:22:47 +0300

    chore: initialize Spring Boot project

---
+++ src/test/java/com/marioschartsias/matches/MatchesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marioschartsias.matches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
